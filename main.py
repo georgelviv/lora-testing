61efb262f0abd7a925599d26a4e331afbc4f34a8
Yurii Shkoropad <lvivgeorge@gmail.com>
2025-12-07 15:58:17 +0200

    add gitignore

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from lora_simulation import LoraSimulation, LORA_SIMULATION_ENVIRONMENTS
+from lora_configs import lora_configs
 
 def main():
   lora_sim = LoraSimulation(env_model=LORA_SIMULATION_ENVIRONMENTS['open_field'])
-  print(lora_sim)
+  for config in enumerate(lora_configs):
+    lora_sim.set_config(config)
+    print(f"Config {config}")
+    print(f"Result: {lora_sim.ping()}")
 
 if __name__ == "__main__":
   main()
